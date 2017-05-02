dd295e8b72e0612ae7755e7aa54f92fd26496ec4
Mitchel Mayle <mitchel.mayle@gmail.com>
2017-05-02 11:59:13 -0400

    Started gameplay

--- ComputerWars/App_Start/RouteConfig.cs
+++ ComputerWars/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace ComputerWars
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 name: "Default",
--- ComputerWars/Controllers/HomeController.cs
+++ ComputerWars/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComputerWars.Models;
 
 namespace ComputerWars.Controllers
 {
@@ -11,6 +12,21 @@ namespace ComputerWars.Controllers
         public ActionResult Index()
         {
             return View("Index");
+        }
+
+        public ActionResult Game()
+        {
+            if (Session["player"] == null)
+            {
+                Player player = new Player();
+                Session["player"] = player;
+
+                return View("Game", player);
+            }
+            else
+            {
+                return View("Game", Session["player"] as Player);
+            }
         }
     }
 }
